7b729a707e3756900ba049f62404b2086af2ed8c
AaronSBF <aaronkelly883@gmail.com>
2022-10-10 13:59:22 +0000

    fixed footer CSS

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,19 +2,19 @@ import facebook from "../../images/icon-facebook.svg";
 import instagram from "../../images/icon-instagram.svg";
 import pinterest from "../../images/icon-pinterest.svg";
 import twitter from "../../images/icon-twitter.svg";
-import footerCSS from "./footer.css";
+import classes from "./footer.css";
 
 
 const Footer = ()=>{ 
 
   return(
-    <footer className={footerCSS.footer}> 
+    <footer className={classes.footer}> 
       <div className="container row"> 
-        <h2 className={footerCSS.footer__title}>Shortly</h2>
-        <div className={`row ${footerCSS.footer__lists}`}> 
+        <h2 className={classes.footer__title}>Shortly</h2>
+        <div className={`row ${classes.footer__lists}`}> 
           <div> 
             <h3>Features</h3>
-            <ul className={footerCSS.footer__list}> 
+            <ul className={classes.footer__list}> 
               <li>Link Shortening</li>
               <li>Branded Links</li>
               <li>Analytics</li>
@@ -22,7 +22,7 @@ const Footer = ()=>{
           </div>
           <div> 
             <h3>Resources</h3>
-            <ul className={footerCSS.footer__list}> 
+            <ul className={classes.footer__list}> 
               <li>Blog</li>
               <li>Developers</li>
               <li>Support</li>
@@ -30,7 +30,7 @@ const Footer = ()=>{
           </div>
           <div> 
             <h3>Company</h3>
-            <ul className={footerCSS.footer__list}> 
+            <ul className={classes.footer__list}> 
               <li>About</li>
               <li>Our Team</li>
               <li>Careers</li>
@@ -38,7 +38,7 @@ const Footer = ()=>{
             </ul>
           </div>
         </div>
-        <div className={`row ${footerCSS.footer__icons}`}> 
+        <div className={`row ${classes.footer__icons}`}> 
         <img src={facebook} alt="Facebook" aria-hidden="true" />
           <img src={twitter} alt="Twitter" aria-hidden="true" />
           <img src={pinterest} alt="Pinterest" aria-hidden="true" />
